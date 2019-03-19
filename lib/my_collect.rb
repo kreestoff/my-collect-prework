18d403da2bacf65d46d2c2f0403fecdf26c74721
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-19 19:50:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(array)
   i = 0 
   result = []
   while i < array.length 
-  block_given?
-  result << yield(array[i])
-  i += 1 
+    result << yield(array[i])
+    i += 1 
 end
 array
 end
